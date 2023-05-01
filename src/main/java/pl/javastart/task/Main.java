89e89abb8b68b4f227a9f2cebccfd808400634f5
Tomasz Iwanicki <tiko35@o2.pl>
2023-05-01 23:49:51 +0200

    Solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,11 +3,9 @@ package pl.javastart.task;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         TournamentStats tournamentStats = new TournamentStats();
         Scanner scanner = new Scanner(System.in);
         tournamentStats.run(scanner);
     }
-
 }
